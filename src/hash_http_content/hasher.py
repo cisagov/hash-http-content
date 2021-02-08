c989d96eae89e02f966a082fc9f6171e511188fa
Nicholas McDonnell <50747025+mcdonnnj@users.noreply.github.com>
2021-02-08 15:52:37 -0500

    Add utility methods for hashing
    Add utility methods to get a desired hashing object and to update and get the
    digest from a hashing object.

--- src/hash_http_content/hasher.py
+++ src/hash_http_content/hasher.py
@@ -1,1 +1,30 @@
-"""Functionality to get a hash of URL's visible content."""
+"""Functionality to get a hash of an HTTP URL's visible content."""
+
+# Standard Python Libraries
+import hashlib
+
+
+def get_hasher(hash_algorithm: str) -> "hashlib._Hash":
+    """Get a hashing object."""
+    # Not all implementations support the "usedforsecurity" keyword argument,
+    # which is used to indicate that the algorithm is being used for non-security
+    # related tasks. This is required for some algorithms on FIPS systems.
+    try:
+        hasher = getattr(hashlib, hash_algorithm)(usedforsecurity=False)
+    except AttributeError:
+        # There is no named constructor for the desired hashing algorithm
+        try:
+            # Work around typeshed's incorrect type hints
+            hasher = getattr(hashlib, "new")(hash_algorithm, usedforsecurity=False)
+        except TypeError:
+            hasher = hashlib.new(hash_algorithm)
+    except TypeError:
+        hasher = getattr(hashlib, hash_algorithm)()
+    return hasher
+
+
+def get_hash_digest(hash_algorithm: str, contents: bytes) -> str:
+    """Get a hex digest representing a hash of the given contents."""
+    hasher: "hashlib._Hash" = get_hasher(hash_algorithm)
+    hasher.update(contents)
+    return hasher.hexdigest()
